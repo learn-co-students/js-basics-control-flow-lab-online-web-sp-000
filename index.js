c43b732d5ff955c0157e9230afbd004787b71f09
Douglas Uretsky <douglasuretsky@gmail.com>
2019-02-19 18:33:11 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code in this file!
+let response;
+
+
+function scuberGreetingForFeet(someValue) {
+  if (someValue <= 400) {
+    response = 'This one is on me!';
+  } else if(someValue > 2500) {
+    response = "No can do.";
+  } else if(someValue > 2000) {
+    response = "I will gladly take your thirty bucks.";
+  }
+}
+
+scuberGreetingForFeet(199)
+
+response
