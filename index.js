07dd2975715bb589828c395b9960eb5c25dca077
Aaron Berns <anberns@gmail.com>
2019-02-23 10:47:11 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your code in this file!
+let scuberGreetingForFeet = (distance) => {
+  if (distance <= 400) {
+    return "This one is on me!";
+  }
+  else if (distance > 2000 && distance <= 2500) {
+    return "I will gladly take your thirty bucks."
+  }
+  else {
+    return "No can do.";
+  }
+}
+
+let ternaryCheckCity = (destination) => {
+  let result = "";
+  destination === "NYC" ? result = "Ok, sounds good." : result = "No go.";
+  return result;
+}
+
+let switchOnCharmFromTip = (tip) => {
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
+}
