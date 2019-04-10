1af4394e844b8046a02e295a8374136467b87d0c
Andrew Waller <andrewhwaller@gmail.com>
2019-04-10 18:40:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your code in this file!
+
+function scuberGreetingForFeet(distance) {
+
+  if (distance <= 400) {
+    return "This one is on me!";
+  } else if (distance > 1999 && distance < 2501) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
+}
+
+function ternaryCheckCity(city) {
+
+  if (city === "NYC") {
+    return "Ok, sounds good.";
+  } else if (city != "NYC") {
+    return "No go.";
+  }
+}
+
+function switchOnCharmFromTip(tip) {
+
+  if (tip === 'generous') {
+    return "Thank you so much.";
+  } else if (tip === 'not as generous') {
+    return "Thank you.";
+  } else {
+    return "Bye.";
+  }
+}
