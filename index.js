bfcb6eae00ce8c0bb2510f5ba2006740fe158214
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-06-24 18:01:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-// // Write your code in this file!
-// scuberGreetingForFeet(distance) {
-//   if (distance < 400) {
-//     return "This one is on me!";
-//   } else if (distance < 2000) {
-//     return "I will gladly take your thirty bucks.";
-//   } else {
-//     return "No can do";
-//   }
-// }
-//
-// ternaryCheckCity(destination) {
-//   return destination === 'NYC' ? "Okay, sounds good." : "No go.";
-// }
-//
-// switchOnCharmFromTip(tip) {
-//   let response;
-//   switch(tip) {
-//     case "generous":
-//       response = "Thank you so much."
-//       break;
-//     case "not as generous":
-//       response = "Thank you.";
-//     default:
-//       response = "Bye.";
-//   }
-//
-//   return response;
-// }
+// Write your code in this file!
+scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+    return "This one is on me!";
+  } else if (distance > 2000 && distance < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do";
+  }
+}
+
+ternaryCheckCity(destination) {
+  return destination === 'NYC' ? "Okay, sounds good." : "No go.";
+}
+
+switchOnCharmFromTip(tip) {
+  let response;
+  switch(tip) {
+    case "generous":
+      response = "Thank you so much."
+      break;
+    case "not as generous":
+      response = "Thank you.";
+    default:
+      response = "Bye.";
+  }
+
+  return response;
+}
