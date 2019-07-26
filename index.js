bf7d714300f45b8f524208750827db07f4b9c35d
Tommy Curley <thomas.curley93@gmail.com>
2019-07-26 14:47:37 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your code in this file!
+function scuberGreetingForFeet(feet) {
+  let result;
+  if (feet < 400) {
+    result = "This one is on me!";
+  } else if (feet > 2000 && feet < 2500) {
+    result = "I will gladly take your thirty bucks.";
+  } else if (feet > 2500) {
+    result = "No can do.";
+  }
+  return result;
+}
+
+function ternaryCheckCity(city) {
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
+}
+
+function switchOnCharmFromTip(tip) {
+  let result;
+  if (tip === "generous") {
+    result = "Thank you so much.";
+  } else if (tip === "not as generous") {
+    result = "Thank you.";
+  } else {
+    result = "Bye.";
+  }
+  return result;
+}
