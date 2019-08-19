f59072542a9579710d49cef211d02d4bd22d09a1
Jared Grove <jaredkgrove@gmail.com>
2019-08-19 14:04:08 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your code in this file!
+function scuberGreetingForFeet(rideLength) {
+    let message;
+    if (rideLength > 2500){
+        message = "No can do."
+    } else if(rideLength > 2000){
+        message = "I will gladly take your thirty bucks.";
+    } else if(rideLength <= 400){
+        message = "This one is on me!";
+    }
+
+    return message;
+};
+
+function ternaryCheckCity(city){
+    let message;
+    if(city === 'NYC'){
+        message = 'Ok, sounds good.'
+    }else {
+        message ='No go.'
+    }
+    return message;
+};
+
+function switchOnCharmFromTip(tip){
+    let message;
+    switch (tip){
+        case "generous":
+            message = "Thank you so much.";
+            break; 
+        case "not as generous":
+            message = 'Thank you.';
+            break;
+        default:
+            message = "Bye."
+            break;
+    }
+
+    return message;
+}
