b5ffd5156da9856a07d9b8c09881fe4eb752e35c
Omar Gonzalez <ogonzaleznyc@gmail.com>
2019-09-08 00:36:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 // Write your code in this file!
-function scuberGreetingForFeet(feet) {
-  if (feet <= 400) {
-    return "This one is on me!"
-  } else if (feet > 2000 && feet <= 2500) {
-    return 'I will gladly take your thirty bucks.'
-  } else { 
-    return "No can do."
-  }
+function scuberGreetingForFeet(distance) { 
+  if (distance <= 400) {return 'This one is on me!'}
+  else if (distance > 2000 && distance <= 2500) {return 'I will gladly take your thirty bucks.'}
+  else if (distance > 2500) {return 'No can do.'}
 }
 
 function ternaryCheckCity(city) {
-  return city == 'NYC' ? 'Ok, sounds good.' : 'No go.'
+  let response
+  city === 'NYC' ? response = "Ok, sounds good." : response = "No go.";
+  return response;
 }
+//returns "Ok, sounds good." when the city is NYC
+//should return "No go." if the destination city is not NYC
 
 function switchOnCharmFromTip(tip) {
+  //let response;
   switch (tip) {
-    case 'generous':
-  return 'Thank you so much.';
-    case 'not as generous':
-  return 'Thank you.';
-    case 'thanks for everything':
-  return 'Bye.';
-  }
+  case 'generous':
+    //response = "Thank you so much."
+  return "Thank you so much.";
+  //break;
+  case 'not as generous':
+    //response = "Thank you."
+  return "Thank you.";
+  //break;
+  default:
+  //response = "Bye."}
+  //return response;
+  return "Bye."}
 }
