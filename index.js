99d0f17301d7ade8c5d9fa55746b10a827b9b515
Alan Krajina <alankrajina88@gmail.com>
2019-09-09 13:05:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(number){
+  if (number <= 400){
+    return "This one is on me!";
+  } else if (number > 2000 && number < 2500){
+      return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+
+function ternaryCheckCity(city){
+  let canTravel;
+   city === "NYC" ? (canTravel='Ok, sounds good.') : (canTravel='No go.');
+   return canTravel
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+      case 'generous':
+          return 'Thank you so much.'
+          break;
+      case 'not as generous':
+          return 'Thank you.'
+          break;
+      default:
+      return 'Bye.'
+  }}
