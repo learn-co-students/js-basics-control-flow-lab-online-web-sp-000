35581de03757aa0a45c20ff9a28203e61d1b35ad
ch2isk4kos <ch2isk4kos@gmail.com>
2019-09-10 17:53:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(density_level){
+    if (density_level <= 400) {
+        return "This one is on me!";
+    } else if (density_level < 2500) {
+        return "I will gladly take your thirty bucks.";
+    } else {
+        return "No can do.";
+    }
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+    return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(charm){
+    switch (charm){
+        case "generous":
+            return "Thank you so much."
+            break;
+        case "not as generous": // case !"generous":
+            return "Thank you."
+            break;
+        default:
+            return "Bye.";
+    }
+
 }
