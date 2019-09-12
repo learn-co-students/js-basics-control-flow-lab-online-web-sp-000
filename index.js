a934f78dfaccd6ba2a8a37d48c5355f850ef4667
donna <donnacamos88@gmail.com>
2019-09-12 19:47:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
-}
+  let response;
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+  if(distance <= 400){
+    response = "This one is on me!"
+  } else if (distance > 2000) {
+    if (distance > 2500) {
+      response = "No can do."
+    } else {
+      response = "I will gladly take your thirty bucks."
+    }
+  };
 
-function switchOnCharmFromTip(){
+  return response;
+};
+
+
+function ternaryCheckCity(city){
   // Write your code here!
-}
+  const response = city === 'NYC' ? "Ok, sounds good." : "No go."
+
+  return response;
+}; 
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  let response;
+
+  switch(tip) {
+    case 'generous':
+      response = "Thank you so much."
+      break;
+      case 'not as generous':
+        response = "Thank you."
+        break;
+      default:
+        response = 'Bye.'
+  };
+  return response;
+};
