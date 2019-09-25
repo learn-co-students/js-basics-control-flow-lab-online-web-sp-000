80249d9966f9b551db5d2d044a432620c8317693
Irina Kagan <irinagamb@gmail.com>
2019-09-25 00:45:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(numberOfFeet){
   // Write your code here!
+  let reply;
+
+  if (numberOfFeet  <= 400) {
+    reply = "This one is on me!";
+  } else if (numberOfFeet > 2000 && numberOfFeet < 2500) {
+    reply = "I will gladly take your thirty bucks.";
+  } else if (numberOfFeet > 2500) {
+    reply = "No can do.";
+  }
+  return reply
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  switch (city) {
+    case 'NYC':
+       return 'Ok, sounds good.'
+      break;
+      default:
+     return 'No go.'
+      break;
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+       return 'Thank you so much.'
+      break;
+    case 'not as generous':
+     return 'Thank you.'
+      break;
+    default:
+     return 'Bye.'
+      break;
+  }
 }
